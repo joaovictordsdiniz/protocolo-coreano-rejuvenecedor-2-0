de2d21cd23c05745533545aacdc02ff5360d271f
João Victor de Souza Diniz <76671489+joaovictordsdiniz@users.noreply.github.com>
2023-10-24 21:57:08 -0300

    Update

--- video-3/script.js
+++
@@ -1,6 +0,0 @@
-const dataAtual = new Date();
-const dataFormatada = `${dataAtual.getDate()}/${dataAtual.getMonth() + 1}/${dataAtual.getFullYear()}`;
-const spanData = document.getElementById('dataAtual');
-spanData.textContent = dataFormatada;
-
-if (!document.querySelector('script[src^="https://player.pandavideo.com.br/api.v2.js"]')) { let s = document.createElement('script'); s.src = 'https://player.pandavideo.com.br/api.v2.js'; s.async = true; document.head.appendChild(s); } window.pandascripttag = window.pandascripttag || []; window.pandascripttag.push(function () { const p = new PandaPlayer('panda-d6436af5-0bac-4692-9109-c6af27f13ea1', { onReady() { p.loadButtonInTime({ fetchApi: true }) } }); })
